c415fc0ae2b5f3d79748419e131def4718e93ce6
SabariGanesh <temp1@sabari-zsch894.csez.zohocorpin.com>
2021-08-05 18:52:59 +0530

    Practiced lecture 6

--- Implementing NameCalculator Example in AngularJS - 6/JS/app.js
+++ Implementing NameCalculator Example in AngularJS - 6/JS/app.js
@@ -1,6 +1,16 @@
+// 'use strict'
 (function(){
-    angular.module("MyFirstApp",[]).controller("MyFirstController",function($scope){
+    angular.module("Name Calculator",[]).controller("nameCalculatorController",function($scope){
         $scope.name = "";
-        $scope.greetings = name => `Hello ${name}`;
-    });   
+        $scope.result = 0;
+        $scope.displayResult = () => {
+            $scope.result = calculation($scope.name);
+        }
+        let calculation = name => {
+            let sumUp = 0;
+            for(let i=0;i<name.length;i++)
+                sumUp += name.charCodeAt(i);
+            return sumUp;  
+        }
+    }); 
 })();
