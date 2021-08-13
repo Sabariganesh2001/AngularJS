610baa3df2d2677a58c3869a6b9d67f85b90cd8b
SabariGanesh <temp1@sabari-zsch894.csez.zohocorpin.com>
2021-08-13 22:05:01 +0530

    Practiced the lecture 11

--- 11 - Expressions and Interpolation(1)/JS/app.js
+++ 11 - Expressions and Interpolation(1)/JS/app.js
@@ -1,5 +1,9 @@
-'use strict'
 (function(){
-    angular.module("MyFirstApp",[])
-    .controller("MyFirstController");
+    'use strict' 
+    let msg = function($scope){
+        $scope.message = "Hello everyone, Hope all are fine."
+        $scope.name = "SabariGanesh";
+    }
+    angular.module("MsgApp",[]).controller("MsgAppController",msg);
+    msg.$inject=['$scope']; //To protect the dependencies from minifier     
 })();
