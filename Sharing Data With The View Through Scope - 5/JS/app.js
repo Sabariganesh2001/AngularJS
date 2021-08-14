ffb2124d46825544265c932dc58ecfa579f1eaa7
SabariGanesh <temp1@sabari-zsch894.csez.zohocorpin.com>
2021-08-14 12:58:52 +0530

    Deleted the duplicate file

--- Sharing Data With The View Through Scope - 5/JS/app.js
+++
@@ -1,6 +0,0 @@
-(function(){
-    angular.module("MyFirstApp",[]).controller("MyFirstController",function($scope){
-        $scope.name = "";
-        $scope.greetings = name => `Hello ${name}`;
-    });   
-})();
