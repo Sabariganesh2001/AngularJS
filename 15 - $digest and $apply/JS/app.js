bf80fb1328e73543c411cbaa3bccbb0dc4439bb0
SabariGanesh <temp1@sabari-zsch894.csez.zohocorpin.com>
2021-08-26 15:48:30 +0530

    Practiced and understood the $digest() and $apply()

--- 15 - $digest and $apply/JS/app.js
+++ 15 - $digest and $apply/JS/app.js
@@ -17,26 +17,39 @@
         // In this type interpolation will not work, The reason is timeout gets put in the event Queue completely separately from this call of incrementCounter really gets it taken out out of the angular context all together. It's not being called inside the angular context. And because it's not being called inside the angular context the digest cycle doesn't know to have to To kick off at all.
         
         
-        // 2) Run digest cycle manually by using $digest     
+        // 2) Run digest cycle manually by using $digest function     
+            // let counterControllerFunction = function(scope){
+            //     scope.counter = 0; 
+            //     scope.incrementCounter = function(){
+            //         setTimeout(function(){
+            //             scope.counter++;
+            //             scope.$digest(); //here we run the digest cycle manually
+            //             console.log("Counter is incremented") 
+            //         },2000);                
+            //     }  
+            // }
+            // angular.module("CounterApp",[]).controller("CounterController",['$scope',counterControllerFunction]);
+
+            //The problem with colon (within the function) $digest directly is if there is any kind of errors or any exceptions that happen in the colon that we are executing, the exceptions thrown as part of this code will not be visible to Angular JS. To overcome this disadvantage we can use $apply
+            
+        // 3) Using $apply function 
         let counterControllerFunction = function(scope){
             scope.counter = 0; 
             scope.incrementCounter = function(){
                 setTimeout(function(){
-                    scope.counter++;
-                    scope.$digest(); //here we run the digest cycle manually
-                    console.log("Counter is incremented") 
-                },2000)
-                
-            }
-            // $("#demo").keydown(function(e){
-            //     console.log(e.key);   
-            // })     
-        }
-        angular.module("CounterApp",[]).controller("CounterController",['$scope',counterControllerFunction]);
+                    scope.$apply(function(){
+                        scope.counter++;
+                        console.log("Counter is incremented") 
+                    });
+                },2000)              
+            }      
+        }        
+        angular.module("CounterApp",[]).controller("CounterController",['$scope',counterControllerFunction]);   
 
+        //So now, when the time out gets executed after two seconds, this function will get executed, and this function will call $apply. And have another function as part of it and execute that function, which will have the code that we actually want to execute. But since it's all going to get wrapped inside the apply function, not only will we catch any exceptions in AngularJSSo now, when the time out gets executed after two seconds, this function will get executed, and this function will call $apply. And have another function as part of it and execute that function, which will have the code that we actually want to execute. But since it's all going to get wrapped inside the apply function, not only will we catch any exceptions in AngularJS  But also at the end of that process, the digest, $digest will get called automatically by angular, and therefore they will get updated. 
 
+        //The $scope.$apply() function in angularjs is used whenever we want to integrate any other code with angularjs. Generally the $scope.$apply() function will execute custom code and then it will call $scope.$digest() function forcefully to check all watch list variables and update variable values in view if any changes found for watch list variables.
 
-    //Doubt:
-        // How the $watch(in previous lecture) and $digest function is available in $scope (as it doesn't show this function is console).
+        //In angularjs $scope object is having different functions like $watch(), $digest() and $apply() and we will call these functions as central functions. The angularjs central functions $watch(), $digest() and $apply are used to bind data to variables in view and observe changes happening in variables.
 
 })();
